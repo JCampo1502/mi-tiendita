815b3d9dcac773e091ea4a35aa1d1768cb4fb4ce
Javier D. Campo <javierdavidcamposuarez15@gmail.com>
2024-07-28 03:12:13 -0500

    feat: :sparkles: add city sellector

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@ import { getProductById } from "./modules/product/getProducts";
 import { addProductStorageObserver, getStorageProducts } from "./modules/product/productStorage";
 import { cartCardTemplate } from "./modules/product/templates/cartCardTemplate";
 import { emptyCart } from "./modules/product/templates/emptyCart";
+import * as bootstrap from 'bootstrap'
 
 const main = document.querySelector(".main");
 const detailModal = document.querySelector("#detail__modal");
@@ -12,9 +13,17 @@ const locationForm = document.querySelector('.location__form');
 const cartContainers = document.querySelectorAll(".cart__container");
 const cartAmount = document.querySelectorAll('.cart__amount');
 const cartTotal = document.querySelectorAll('.cart__total');
-const locationName = document.querySelectorAll('.location__city');
+const locationNames = document.querySelectorAll('.location__city');
 
-
+const updateLocationNames = ()=>{
+    const newLocation = localStorage.getItem('location');
+    if(!newLocation){
+        const modal = new bootstrap.Modal(document.querySelector("#locationModal"));
+        modal.show();        
+    }
+    locationNames.forEach(element => element.textContent = newLocation);
+    locationForm.location.value = newLocation;
+}
 
 const loadTotalAccount = async(localProduct = new Map())=>{
     const products = await Promise.all(
@@ -57,6 +66,7 @@ const loadCartCards = async(localProducts= new Map())=>{
 /* start actions */
 window.addEventListener('DOMContentLoaded',()=>{    
     const localProducts = getStorageProducts();
+    updateLocationNames();
     loadInitialImages();
     loadTotalAccount(localProducts);
     loadCartCards(localProducts)
@@ -65,7 +75,15 @@ window.addEventListener('DOMContentLoaded',()=>{
     addProductStorageObserver(loadTotalAccount);    
     addProductStorageObserver(loadCartCards);  
     locationForm.addEventListener('submit', ()=>{
-
+        const value = locationForm.location.value;
+        localStorage.setItem('location',value);
     }) 
+
+    window.addEventListener('storage', e=>{
+        if(e.key === 'location'){
+            updateLocationNames();
+        }
+    })
+
 })
 
